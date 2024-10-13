fbfbb42f4a471162a81f1560aa91af57db4cf071
ambiso <ambisotopy@gmail.com>
2024-10-13 20:08:18 +0200

    remove unsafe

--- src/lib.rs
+++ src/lib.rs
@@ -283,15 +283,13 @@ pub mod parse_simd {
     //     line_number
     // }
     pub fn parse_template(input: &[u8]) -> usize /* Vec<NumberedBlock<'_>> */ {
-        // input.iter().filter(|&&x| x == b'\n').count()
         let mut line_number = 0usize;
 
-        let (head, middle, tail) = unsafe { input.align_to::<Wide>() };
+        let (head, middle, tail) = input.as_simd();
 
         line_number += head.iter().filter(|&&x| x == b'\n').count();
         for c in middle.iter() {
-            // let newlines = cs.simd_eq(Wide::splat(b'\n')).to_bitmask().count_ones();
-            let newlines = c.simd_eq(Wide::splat(b'\n')).to_int().reduce_sum().neg();
+            let newlines = c.simd_eq(Wide::splat(b'\n')).to_bitmask().count_ones();
             line_number += newlines as usize;
         }
         line_number += tail.iter().filter(|&&x| x == b'\n').count();
